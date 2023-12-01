7cdda1890babfa346110ffc2dfd7a7b822c6964c
HariharanJ2000 <hariharanasjh@gmail.com>
2023-12-01 07:44:50 +0530

    generated access and refresh token

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -2,10 +2,53 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const axios = require('axios')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+const secretKey = 'asjh'
+
+const generateAccessToken = (props) => {
+    const payload = {
+        email: props.email,
+        sub: props.sub,
+        name: props.name
+    }
+    const options = {
+        expiresIn: '30m'
+    }
+    console.log(jwt.sign(payload,secretKey,options));
+    return jwt.sign(payload,secretKey,options)
+}
+const generateRefreshToken = (props) => {
+    const payload = {
+        email: props.email,
+        sub: props.sub,
+        name: props.name
+    }
+    const options = {
+        expiresIn: '1d'
+    }
+    console.log(jwt.sign(payload,secretKey,options));
+    return jwt.sign(payload,secretKey,options)
+}
+
+const verifyAccessToken = (token) => {
+    try{
+        const decoded = jwt.verify(token,secretKey)
+        return {
+            success: true,
+            data: decoded
+        }
+    }catch(err){
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+}
 
 app.post('/login', async (req, res) => {
     const { grantType } = req.body
@@ -27,7 +70,13 @@ app.post('/login', async (req, res) => {
                     "Authorization": `Bearer ${access_token}`
                 }
             }).then(apires => {
-                console.log(apires)
+                console.log(apires.data)
+                if(apires.data.email_verified){
+                    response.accessToken = generateAccessToken(apires.data)
+                    response.refreshToken = generateRefreshToken(apires.data)
+                    response.userName = apires.data.name
+                }
+                console.log(response,{});
             }).catch(err => {
                 console.log(err);
             })
@@ -38,10 +87,10 @@ app.post('/login', async (req, res) => {
         case 'meta':
 
             break;
-
         default:
             break;
     }
+    console.log(response);
     res.status(200).send(response)
 })
 
